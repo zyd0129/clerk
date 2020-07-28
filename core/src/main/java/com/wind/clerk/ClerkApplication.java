aadbf19563797891ac8ee34431289f7c34b23da8
zyd0129 <zyd_0129@qq.com>
2020-07-28 21:07:10 +0800

    init project

---
+++ core/src/main/java/com/wind/clerk/ClerkApplication.java
@@ -0,0 +1,11 @@
+package com.wind.clerk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClerkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClerkApplication.class, args);
+    }
+}
