5454c47b52f7a290aeacfc9c7bb503eeb53bfa98
zyd0129 <zyd_0129@qq.com>
2020-08-03 09:18:26 +0800

    模块及微服务划分

---
+++ clerk-web/src/main/java/com/wind/clerk/controller/Demo.java
@@ -0,0 +1,27 @@
+package com.wind.clerk.controller;
+
+import org.apache.catalina.core.AsyncContextImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.AsyncContext;
+import java.util.concurrent.FutureTask;
+
+@RestController
+public class Demo {
+    @Value("${user.name}")
+    String name;
+
+
+    @Value("${user.nickName}")
+    String nickName;
+
+    @GetMapping("hello")
+    public String hello() {
+//        FutureTask futureTask = new FutureTask(s -> s);
+//        AsyncContext asyncContext = new AsyncContextImpl();
+//        asyncContext.setTimeout();
+        return "hello," + nickName;
+    }
+}
