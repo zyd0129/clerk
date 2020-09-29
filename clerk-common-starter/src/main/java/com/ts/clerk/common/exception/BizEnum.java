7b269da5406e3678ac9932b76ce137f926dcf0ae
yds <yandongsheng@adpanshi.com>
2020-09-29 10:49:52 +0800

    BizEnum.java添加枚举值

--- clerk-common-starter/src/main/java/com/ts/clerk/common/exception/BizEnum.java
+++ clerk-common-starter/src/main/java/com/ts/clerk/common/exception/BizEnum.java
@@ -1,8 +1,22 @@
 package com.ts.clerk.common.exception;
 
+/**
+ * BizEnum
+ */
 public enum BizEnum {
+    /**
+     * BizEnum
+     */
+    ERROR(-1, "操作失败"),
+    SUCCESS(0, "操作成功"),
     USER_PASSWORD_INCONSISTENCY(6001, "password inconsistency"),
-    USER_NOT_EXIST(6002, "user not exist");
+    USER_NOT_EXIST(6002, "user not exist"),
+
+    //---------------------------JURY-------------------------------------------//
+    JURY_ERROR(9000, "jury unknown error"),
+    JURY_FAILED(9001, "jury response failed"),
+    JURY_VALIDATE_FAILED(9002, "jury parameter verification failed");
+
     private Integer code;
     private String message;
 
--- clerk-common-starter/src/main/java/com/ts/clerk/common/response/ApiResponse.java
+++ clerk-common-starter/src/main/java/com/ts/clerk/common/response/ApiResponse.java
@@ -1,7 +1,10 @@
 package com.ts.clerk.common.response;
 
+import com.ts.clerk.common.exception.BizEnum;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -10,53 +13,72 @@ import java.io.Serializable;
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiResponse<T> implements Serializable {
     private static final long serialVersionUID = 7614396417801217389L;
+    private static final int SUCCESS = 0;
+    private static final int ERROR = -1;
+    ApiResponse<T> juryApiResponse;
     /**
      * 返回码 0：成功，其他为错误
      */
     private int code;
- 
     /**
      * 错误消息
      */
     private String message;
- 
     /**
      * 正确时返回的数据
      */
     private T data;
- 
-    private static final int SUCCESS = 0;
- 
-    public boolean isSuccess() {
-        return code == 0;
+
+    public static <T> ApiResponse<T> error(BizEnum bizEnum) {
+        return ApiResponse.<T>builder()
+                .code(bizEnum.getCode())
+                .message(bizEnum.getMessage())
+                .build();
     }
- 
-    public static ApiResponse error(int errorCode, String msg) {
-        ApiResponse apiResponse =  ApiResponse
-                .builder()
-                .code(errorCode)
+
+    public static <T> ApiResponse<T> error(BizEnum bizEnum, String msg) {
+        return ApiResponse.<T>builder()
+                .code(bizEnum.getCode())
                 .message(msg)
                 .build();
-        return apiResponse;
     }
- 
-    public static<T> ApiResponse<T> success(T data) {
-        ApiResponse apiResponse =  ApiResponse
-                .builder()
+
+    public static <T> ApiResponse<T> error(T data) {
+        return ApiResponse.<T>builder()
+                .code(BizEnum.ERROR.getCode())
+                .message(BizEnum.ERROR.getMessage())
+                .data(data)
+                .build();
+    }
+
+
+    public static <T> ApiResponse<T> error(int resultCode, String msg) {
+        return ApiResponse.<T>builder()
+                .code(resultCode)
+                .message(msg)
+                .build();
+    }
+
+
+    public static <T> ApiResponse<T> success() {
+        return ApiResponse.<T>builder()
+                .code(SUCCESS)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return ApiResponse.<T>builder()
                 .code(SUCCESS)
                 .data(data)
                 .build();
-        return apiResponse;
     }
- 
-    public static ApiResponse success() {
-        ApiResponse apiResponse =  ApiResponse
-                .builder()
-                .code(SUCCESS)
-                .build();
-        return apiResponse;
+
+    public boolean isSuccess() {
+        return code == 0;
     }
 
 }
